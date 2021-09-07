d71ef0572d8ec237eb0ca5368c20201eadd7c487
Felipe Martins <fmartins809@gmail.com>
2021-09-07 12:02:18 -0300

    Project created

---
+++ backend/src/main/java/com/martins/mavendas/MavendasApplication.java
@@ -0,0 +1,13 @@
+package com.martins.mavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/martins/mavendas/MavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martins.mavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
